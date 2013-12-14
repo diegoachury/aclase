5e9f57e80634139d26c74538e803a97bbfad3590
diegoachury <tiran_o@hotmail.com>
2013-12-14 10:06:39 -0500

    creando repositorio a clase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aclase::Application.config.secret_key_base = '44223bb9924c23b4bf3d174c19e3d446f44179e99fbe44251f1172283f01699125d192f1690cbe87f911dc4c06012ee7d0578a7576c19f63378c03fd331f8c7c'
